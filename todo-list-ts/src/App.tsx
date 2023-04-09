113599b13cfea0633d7faf58581a0ea3c017965b
Francisco Xavier <fxsjunior@gmail.com>
2023-04-09 13:49:48 -0300

    construindo o css

--- todo-list-ts/src/App.tsx
+++ todo-list-ts/src/App.tsx
@@ -1,5 +1,6 @@
-import './App.css'
+import './styles/App.css'
 import todoLogo from './assets/todo-logo.svg.svg'
+import emptyTask from './assets/empty.tasks.svg.svg'
 import { VscAdd } from "react-icons/vsc";
 import { BsCircle } from "react-icons/bs";
 import { BsTrash3Fill } from "react-icons/bs";
@@ -7,35 +8,37 @@ import { BsTrash3Fill } from "react-icons/bs";
 
 export function App() {
   const addTask = () => {
-    console.log("fui cli")
+    console.log("fui clicado")
   }
 
   return (
     <div className="App">
       <div className='wrapper'>
-        <header>
+        <header className='header'>
           <img src={todoLogo} alt='Logo todo lsit' />
         </header>
 
-        <main>
-          <form action="">
-            <input type="text" placeholder='Adicione uma nova tarefa' />
-            <button type='submit' onClick={() => addTask} >Criar <VscAdd size={20} /></button>
+        <main className='main'>
+          <form action="" className='form'>
+            <input type="text" placeholder='Adicione uma nova tarefa' className='input' />
+            <button type='submit' className='botaoCriar' onClick={() => addTask} >Criar <VscAdd size={20} /></button>
           </form>
 
+
+
           <div className="task-container">
-            <div>
+            <div className='tarefas'>
               <p>Tarefas criadas</p>
               <span>0</span>
             </div>
-            <div>
-              <p>tarefas concluídas</p>
+            <div className='tarefas'>
+              <p>Tarefas concluídas</p>
               <span>0</span>
             </div>
           </div>
 
-          <div className="empty_task">
-            <img src="" alt="" />
+          <div className="empty-task">
+            <img src={emptyTask} alt="" />
             <div>
               <p>Você ainda não tem tarefas cadastradas</p>
               <p>Crie tarefas e organize seus items a fazer</p>
@@ -44,8 +47,10 @@ export function App() {
 
           <div className="tasks_list">
             <div className="task">
-              <BsCircle />
-              <p>Dar banho nos cachorros</p>
+              <div>
+                <BsCircle />
+                <p>Dar banho nos cachorros</p>
+              </div>
               <BsTrash3Fill />
             </div>
           </div>
